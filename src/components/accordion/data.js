9369fe8c0a349744837ad90649328b3026a0bd8b
Lenny Lorenz <lennylorenz88@gmail.com>
2024-05-28 11:42:33 +0200

    Create Accordion Q&A page

---
+++ src/components/accordion/data.js
@@ -0,0 +1,26 @@
+
+const data = [
+    {
+        id : '1',
+        question: 'Who is Lenny Lorenz?',
+        answer: 'Lenny is a young and aspiring student with a dream to one day become a top-level programmer.'
+    },
+    {
+        id : '2',
+        question: 'What inspired him to start coding?',
+        answer: 'He was inspired to start coding because he loves problem-solving and creating things from scratch. The logic and creativity involved in coding really appeal to him, and it is a skill that he can continuously improve and apply in various ways.'
+    },
+    {
+        id : '3',
+        question: 'How does he balance his studies at ESCP with his passion for coding?',
+        answer: 'Balancing his studies and coding requires good time management. He sets specific goals for both his academic work and coding projects, making sure to allocate time for each. This way, he can excel in his studies while also dedicating time to his passion for coding.'
+    },
+    {
+        id : '4',
+        question: 'What coding languages and technologies is he currently working with?',
+        answer: 'He is currently working with JavaScript and React. He enjoys building web applications and exploring new frameworks and libraries to enhance his skills and stay updated with industry trends.'
+    },
+    
+]
+
+export default data;
